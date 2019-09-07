7247f1f03f220207d10bda0d2fde568b0e70d938
KoenBisschop <koen.bisschop@hotmail.be>
2019-09-07 11:13:22 +0200

    class Evaluatie wordt toegevoegd

---
+++ School2020/SchoolDomain/Business/Evaluatie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDomain.Business
+{
+    public class Evaluatie
+    {
+        public String Titel { get; internal set; }
+        public Double Behaald { get; internal set; }
+        public Double Maximum { get; internal set; }
+
+        internal Evaluatie(String titel, Double behaald, Double maximum)
+        {
+            Titel = titel;
+            Behaald = behaald;
+            Maximum = maximum;
+        }
+
+    }
+}
