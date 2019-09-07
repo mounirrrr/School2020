95a5133860f22601e6b71e0625a0b5e2ce96e2d0
KoenBisschop <koen.bisschop@hotmail.be>
2019-09-07 10:28:16 +0200

    Entity Vak added

---
+++ School2020/SchoolDomain/Business/Vak.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDomain.Business
+{
+    public class Vak: Entity
+    {
+        public String Naam { get; internal set; }
+
+        internal Vak( Int32 id, String naam): base(id)
+        {
+            Naam = naam;
+        }
+
+        public override string ToString()
+        {
+            return Naam + ": resultaat onbekend";
+        }
+    }
+}
