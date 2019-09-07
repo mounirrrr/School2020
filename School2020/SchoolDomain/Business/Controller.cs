502575d8c70abdb3b0c3f78a93b4831da600f0fa
KoenBisschop <koen.bisschop@hotmail.be>
2019-09-07 10:56:37 +0200

    Businesscontroller voorziet in de communicatie met de code buiten het project! Dit is nodig omdat wijzigingen anders niet opgeslagen worden in de database. De code in domeinklassen bevat enkel domeinlogica...

--- School2020/SchoolDomain/Business/Controller.cs
+++ School2020/SchoolDomain/Business/Controller.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace SchoolDomain.Business
 {
-    class Controller
+    public class Controller
     {
+        /// <summary>
+        /// _loaded zorgt er voor dat als we 2 controller-objecten aanmaken,
+        /// de vakken van de eerste controller niet verwijderd worden als we de 2de controller aanmaken
+        /// zie constructor en methode Load van de VakRepository!
+        /// </summary>
+        private Boolean _loaded = false;
+
+        public Controller()
+        {
+            if (!_loaded)
+                VakRepository.Load(null);
+            //Wat als we het programma opnieuw opstarten?
+        }
+
+        #region Vakken
+        public void NieuwVak(string naam)
+        {
+            Int32 _id = VakRepository.GetNextId();
+            Vak _vak = new Vak(_id,naam);
+            VakRepository.AddItem(_vak);
+            //Is dit vak opgeslagen in de database?
+        }
+
+        public List<Vak> GetAlleVakken()
+        {
+            return VakRepository.Items;
+        }
+
+        public Vak ZoekVak(string naam)
+        {
+            //Is er werk in de database?
+            return VakRepository.FindItem(naam);
+        }
+        #endregion
     }
 }
