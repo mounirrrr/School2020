0465c8d66b3646ae51ad46ddd551abe64cbe5aef
KoenBisschop <koen.bisschop@hotmail.be>
2019-09-07 10:24:54 +0200

    Entity sterotype added

---
+++ School2020/SchoolDomain/Business/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDomain.Business
+{
+    public class Entity
+    {
+        Int32 id;
+
+        public Int32 Id
+        {
+            get
+            {
+                return id;
+            }
+            internal set
+            {
+                id = value;
+            }
+        }
+
+        internal Entity(Int32 id)
+        {
+            Id = id;
+        }
+    }
+}
