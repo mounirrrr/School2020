0ef6b1635cc8bc8e768ef54707358ab2ca7f5b01
KoenBisschop <koen.bisschop@hotmail.be>
2019-09-29 11:43:13 +0200

    Aggregatie tussen evaluatie en component. Een avaluatie is een taak, een toets, een grote overhoring, een examen... Merk op! De component is geen onderdeel van de evaluatie. De evaluatie verwijst naar de component in de repository!

--- School2020/SchoolDomain/Business/Evaluatie.cs
+++ School2020/SchoolDomain/Business/Evaluatie.cs
@@ -11,12 +11,22 @@ namespace SchoolDomain.Business
         public String Titel { get; internal set; }
         public Double Behaald { get; internal set; }
         public Double Maximum { get; internal set; }
+        public Int32 ComponentId { get; internal set; }
 
-        internal Evaluatie(String titel, Double behaald, Double maximum)
+        public Component Component
+        {
+            get
+            {
+                return ComponentRepository.GetItem(ComponentId);
+            }
+        }
+
+        internal Evaluatie(String titel, Double behaald, Double maximum, Int32 componentId)
         {
             Titel = titel;
             Behaald = behaald;
             Maximum = maximum;
+            ComponentId = componentId;
         }
 
     }
