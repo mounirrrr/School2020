2d5cfabbc4fc0dd90f2690289b3b22cf7713ff52
Koen Bisschop <koen.bisschop@hotmail.be>
2019-09-29 16:45:15 +0200

    Test het ophalen van de vakken uit.

---
+++ School2020/SchoolDomainTests2/Business/ControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchoolDomain.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDomain.Business.Tests
+{
+    [TestClass()]
+    public class ControllerTests
+    {
+        [TestMethod()]
+        public void GetAlleVakkenTest()
+        {
+            Controller c = new Controller();
+            Assert.AreEqual(c.GetAlleVakken().Count,1);
+        }
+    }
+}
