55e1e7e8f11017559ff9aaeb74603d234a0de8ea
KoenBisschop <koen.bisschop@hotmail.be>
2019-09-29 11:34:07 +0200

    Component class added. Component is  a "SELF-SUUFICIENT entity", so it has a repository.

---
+++ School2020/SchoolDomain/Business/Component.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDomain.Business
+{
+    public class Component: Entity
+    {
+        public string Naam { get; private set; }
+        public string Afkorting { get; private set; }
+        public Int32 Gewicht { get; private set; }
+        internal Component(Int32 id, string naam, Int32 gewicht, string afkorting = "") : base(id)
+        {
+            Naam = naam;
+            Gewicht = gewicht;
+            if (afkorting != "")
+                Afkorting = afkorting.ToUpper();
+            else
+            {
+                Afkorting = naam.Substring(0, 2).ToUpper();
+            }
+        }
+    }
+}
---
+++ School2020/SchoolDomain/Business/ComponentRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDomain.Business
+{
+    internal static class ComponentRepository
+    {
+        private static List<Component> _items = new List<Component>();
+        internal static List<Component> Items
+        {
+            get
+            {
+                return _items;
+            }
+            private set
+            {
+                _items = value;
+            }
+        }
+
+        internal static void AddItem(Component item)
+        {
+            Items.Add(item);
+        }
+
+        internal static Component GetItem(Int32 id)
+        {
+            /*
+            foreach (Vak i in Items)
+            {
+                if (i.Id == id) return i;
+            }
+            return null;
+            */
+            return Items.Find(i => i.Id == id);
+        }
+
+        internal static Component FindItem(string naam)
+        {
+            foreach (Component i in Items)
+            {
+                if (i.Naam == naam) return i;
+            }
+            return null;
+
+        }
+
+        internal static void DeleteItem(Int32 id)
+        {
+            Items.Remove(GetItem(id));
+        }
+
+        internal static Int32 GetNextId()
+        {
+            Int32 next_id = 0;
+            foreach (Component item in Items)
+            {
+                if (item.Id > next_id) next_id = item.Id;
+            }
+            return next_id + 1;
+        }
+
+        internal static void Load(List<Component> items)
+        {
+            Items = items;
+        }
+    }
+}
